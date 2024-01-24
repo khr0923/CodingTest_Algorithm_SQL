8d40e9616cd50d3bb65aa76fa86a81d0e2ea2c08
khr0923 <127317034+khr0923@users.noreply.github.com>
2024-01-24 09:46:28 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT o.animal_id, o.animal_type, o.name
+from animal_ins i join animal_outs o
+on i.animal_id = o.animal_id
+where i.sex_upon_intake != o.sex_upon_outcome
+order by o.animal_id;
